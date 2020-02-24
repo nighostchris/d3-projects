df99553b8a1c23c86a1a5f3e07beffe10c900395
Chris <chrisliupascal@gmail.com>
2020-02-24 19:16:32 +0800

    Successful request data from pages/api in index page and pass as props. Going to process soon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+import PolarByCity from '../components/PolarByCity';
 
-const IndexPage = () => {
-  return <div>Welcome to Next.js!</div>
+const IndexPage = (props) => {
+  return <PolarByCity data={props.data} />
 }
+
+IndexPage.getInitialProps =
+  async function() {
+    const res = await axios.get('http://localhost:3000/api/get-sakura-history');
+    const data = await res.data
+
+    console.log(data);
+    return {
+      data: data
+    };
+};
 
 export default IndexPage;
